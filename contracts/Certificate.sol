e11522a78a1489dc91868b0fa5408c6010180300
NGUYỄN BÁ NGỌC <nbngoc.240801@gmail.com>
2023-04-22 15:25:03 +0700

    Certificate

--- contracts/Certificate.sol
+++ contracts/Certificate.sol
@@ -1,18 +1,24 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.18;
 
-import "../interfaces/IUser.sol";
+import "./interfaces/IUser.sol";
+// import "./library/StringArray.sol";
 
 contract Certificate {
     struct AppCertificate {
         string name;
         uint verifiedAt;
         bool exist;
+        string certificateAddress;
+        address candidate;
+        address verifier;
+        DocStatus status;
     }
+
+    enum DocStatus {Pending, Verified, Rejected}
 
     //=============================ATTRIBUTES==========================================
     mapping(uint => AppCertificate) certs;
-    mapping(uint => address) candidateOwnCert;
     IUser user;
 
     constructor(address _userContract) {
@@ -46,13 +52,15 @@ contract Certificate {
 
     error NotCandidate(address user_address);
 
+    error NotVerifier(address user_address);
+
     //=============================METHODs==========================================
     //==================CERTIFICATES=======================
     function isOwnerOfCertificate(
         address _candidateAddress,
         uint _id
     ) public view returns (bool) {
-        return candidateOwnCert[_id] == _candidateAddress;
+        return certs[_id].candidate == _candidateAddress;
     }
 
     // only candidate -> later⏳
@@ -63,7 +71,9 @@ contract Certificate {
         uint _id,
         string memory _name,
         uint _verifiedAt,
-        address _candidateAddress
+        address _candidateAddress,
+        address _verifierAddress,
+        string memory _certificateAddress
     ) public virtual {
         if (certs[_id].exist) {
             revert AlreadyExisted({id: _id});
@@ -74,9 +84,14 @@ contract Certificate {
         ) {
             revert NotCandidate({user_address: _candidateAddress});
         }
+        if (
+            !(user.isExisted(_verifierAddress) &&
+                user.hasType(_verifierAddress, 2))
+        ) {
+            revert NotVerifier({user_address: _verifierAddress});
+        }
 
-        certs[_id] = AppCertificate(_name, _verifiedAt, true);
-        candidateOwnCert[_id] = _candidateAddress;
+        certs[_id] = AppCertificate(_name, _verifiedAt, true, _certificateAddress,_candidateAddress, _verifierAddress, DocStatus.Pending);
 
         AppCertificate memory cert = certs[_id];
 
@@ -89,7 +104,11 @@ contract Certificate {
     function updateCertificate(
         uint _id,
         string memory _name,
-        uint _verifiedAt
+        uint _verifiedAt,
+        address _candidateAddress,
+        address _verifierAddress,
+        string memory _certificateAddress,
+        DocStatus _status
     ) public virtual {
         if (!certs[_id].exist) {
             revert NotExisted({id: _id});
@@ -101,9 +120,13 @@ contract Certificate {
 
         certs[_id].name = _name;
         certs[_id].verifiedAt = _verifiedAt;
+        certs[_id].candidate = _candidateAddress;
+        certs[_id].verifier = _verifierAddress;
+        certs[_id].certificateAddress = _certificateAddress;
+        certs[_id].status = _status;
         AppCertificate memory cert = certs[_id];
 
-        address candidateAddress = candidateOwnCert[_id];
+        address candidateAddress = certs[_id].candidate;
 
         emit UpdateCertificate(
             _id,
@@ -126,10 +149,9 @@ contract Certificate {
         }
 
         AppCertificate memory certificate = certs[_id];
-        address ownerAddress = candidateOwnCert[_id];
+        address ownerAddress = certs[_id].candidate;
 
         delete certs[_id];
-        delete candidateOwnCert[_id];
 
         emit DeleteCertificate(
             _id,
@@ -138,6 +160,27 @@ contract Certificate {
             ownerAddress
         );
     }
+
+//     function getDocument(string memory _certificateAddress) public view 
+//     returns (string memory name, address requester, address verifier, uint verifiedAt, DocStatus status) {
+//         uint index = 0;
+//         AppCertificate memory cert = certs[index];
+//         while(cert.candidate == address(0)) {
+//             if (StringArray.equal(cert.certificateAddress, _certificateAddress)) {
+//                 break;
+//             } else {
+//                 index += 1;
+//                 cert = certs[index];
+//             }
+//         }
+//     return (cert.name, cert.candidate, cert.verifier, cert.verifiedAt, cert.status);
+//   }
+
+  function getDocument(uint _id) public view 
+    returns (string memory name, address requester, address verifier, uint verifiedAt, DocStatus status) {
+        AppCertificate memory cert = certs[_id];
+    return (cert.name, cert.candidate, cert.verifier, cert.verifiedAt, cert.status);
+  }
 
     //======================USER CONTRACT==========================
     function setUserInterface(address _contract) public {
