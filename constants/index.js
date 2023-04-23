1b9a0868cc708b612392ebee0f1e85e90158a8f3
NGUYỄN BÁ NGỌC <nbngoc.240801@gmail.com>
2023-04-23 10:56:24 +0700

    check out

--- constants/index.js
+++
@@ -1,6 +0,0 @@
-const abi = require("./abi.json")
-const contractAddress = require("./contractAddress.json")
-
-module.exports = { abi, contractAddress }
-
-// .json must be created with initial value: {}
--- interfaces/IUser.sol
+++
@@ -1,8 +0,0 @@
-// SPDX-License-Identifier: MIT
-pragma solidity ^0.8.0;
-
-interface IUser {
-    function isExisted(address _userAddress) external view returns (bool);
-
-    function hasType(address _user, uint _type) external view returns (bool);
-}
