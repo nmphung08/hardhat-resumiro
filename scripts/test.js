a516184fb7658bfc3317f482253014bf4beb0528
nmphung08 <nmphung08@gmail.com>
2023-07-12 21:10:59 +0700

    hotfix

--- scripts/test.js
+++ scripts/test.js
@@ -3,24 +3,24 @@ const { ethers, getNamedAccounts } = require("hardhat")
 const { getContracts } = require("./getContracts")
 
 async function main() {
-    // const {
-    //     user,
-    //     company,
-    //     certificate,
-    //     experience,
-    //     job,
-    //     resume,
-    //     skill,
-    //     resumiro,
-    // } = await getContracts()
+    const {
+        user,
+        company,
+        certificate,
+        experience,
+        job,
+        resume,
+        skill,
+        resumiro,
+    } = await getContracts()
 
-    // console.log("All users: ", await resumiro.getAllUser())
-    // console.log("All companies: ", await resumiro.getAllCompanies())
-    // console.log("All jobs: ", await resumiro.getAllJobs())
-    // console.log("All cvs: ", await resumiro.getAllResumes())
-    // console.log("All experiences: ", await resumiro.getAllExperiences())
-    // console.log("All Certificates: ", await resumiro.getAllCertificates())
-    // console.log("All Skills: ", await resumiro.getAllSkill())
+    console.log("All users: ", await resumiro.getAllUser())
+    console.log("All companies: ", await resumiro.getAllCompanies())
+    console.log("All jobs: ", await resumiro.getAllJobs())
+    console.log("All cvs: ", await resumiro.getAllResumes())
+    console.log("All experiences: ", await resumiro.getAllExperiences())
+    console.log("All Certificates: ", await resumiro.getAllCertificates())
+    console.log("All Skills: ", await resumiro.getAllSkill())
 }
 
 main()
